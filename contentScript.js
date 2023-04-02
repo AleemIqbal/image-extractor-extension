86364be9cd497690b6e2ad19daaf0312f1defd1f
Aleem Iqbal <aleemiqbalbhatti@gmail.com>
2023-04-03 00:33:54 +0500

    1

---
+++ contentScript.js
@@ -0,0 +1,15 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'extract_images') {
+    const images = document.querySelectorAll('img:not([src*=".svg"])');
+    const imageData = Array.from(images).map((img) => {
+      const src = img.dataset.src || img.src; // Use the data-src attribute if available
+      return {
+        src,
+        name: src.split('/').pop(),
+        alt: img.alt || 'N/A',
+      };
+    });
+    sendResponse({ imageData });
+  }
+  return true;
+});
---
+++ popup.js
@@ -0,0 +1,84 @@
+document.getElementById('downloadAllButton').addEventListener('click', async () => {
+  const zip = new JSZip();
+  const imageList = document.getElementById('imageList').children;
+  let imageCount = 0;
+
+  for (const li of imageList) {
+    const imageUrl = li.querySelector('.imageItem').title;
+    const imageName = li.querySelector('.imageItem').textContent;
+    try {
+      const response = await fetch(imageUrl);
+      const data = await response.blob();
+      zip.file(imageName, data);
+      imageCount++;
+    } catch (error) {
+      console.error('Failed to fetch image', imageUrl, error);
+    }
+  }
+
+  if (imageCount === 0) {
+    alert('No images found to download.');
+    return;
+  }
+
+  zip.generateAsync({ type: 'blob' }).then((blob) => {
+    saveAs(blob, 'images.zip');
+  });
+});
+document.getElementById('extractButton').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { message: 'extract_images' }, (response) => {
+      const imageList = document.getElementById('imageList');
+      imageList.innerHTML = '';
+	document.getElementById('downloadAllButton').style.display = 'inline-block';
+
+response.imageData.forEach((img) => {
+  const li = document.createElement('li');
+
+const imgPreview = document.createElement('img');
+imgPreview.src = img.src;
+imgPreview.classList.add('imagePreview');
+  
+  const imgName = document.createElement('div');
+  imgName.textContent = img.name;
+  imgName.title = img.src;
+  imgName.classList.add('imageItem');
+
+  const altText = document.createElement('div');
+  altText.textContent = `Alt: ${img.alt}`;
+  altText.classList.add('altText');
+
+  const downloadIcon = document.createElement('span');
+  downloadIcon.classList.add('downloadIcon');
+  downloadIcon.textContent = '⬇️';
+  downloadIcon.title = 'Download image';
+  downloadIcon.addEventListener('click', () => {
+    const link = document.createElement('a');
+    link.href = img.src;
+    link.download = img.name;
+    link.click();
+  });
+
+  li.appendChild(imgPreview);
+  li.appendChild(imgName);
+  li.appendChild(altText);
+  li.appendChild(downloadIcon);
+  imageList.appendChild(li);
+  
+  const copyIcon = document.createElement('span');
+  copyIcon.classList.add('copyIcon');
+  copyIcon.textContent = '📋';
+  copyIcon.title = 'Copy image URL';
+  copyIcon.addEventListener('click', () => {
+    navigator.clipboard.writeText(img.src).then(() => {
+      console.log('Image URL copied to clipboard');
+    }, (error) => {
+      console.error('Failed to copy image URL', error);
+    });
+  });
+
+  li.appendChild(copyIcon);
+});
+    });
+  });
+});
